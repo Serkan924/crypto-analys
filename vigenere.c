89ccf03570389d35c6493c96909548b1116f10f1
Serkan924 <serkandilber12@gmail.com>
2026-03-10 14:30:22 +0200

    vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "vigenere.h"
 
+void encrypt(char *text, char*key) {
+
+  int textLen = strlen(text);
+int keyLen = strlen(key);
+
+for(int i = 0; i < textLen; i++)
+
+  int p = text[i] - 'A';
+int k = key[i % keyLen] - 'A';
+
+char c = (p + k) % 26 + 'A';
+text[i] = c;
+
+}
+decrypt(char *text, char *key) {
+int textLen = strlen(text);
+int keyLen = strlen(key);
+
+for(int i = 0; i < textLen; i++) {
+
+int c = text[i] - 'A';
+int k = key[i % keyLen] - 'A';
+
+char p = (c - k +26) % 26 + 'A';
+text[i] = p;
+}
